10a6ede53a04c8691f5f1ed67e8395fd528d2105
RaiaN <raianknight@gmail.com>
2015-09-28 14:32:07 +0300

    Add preprocess script

---
+++ preprocess_images.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+from scipy import ndimage as ndi
+from skimage.io import imread, imsave
+from skimage import color, img_as_float
+from skimage.filters.rank import autolevel, autolevel_percentile, median, mean, mean_bilateral, entropy
+from skimage.morphology import disk
+
+
+IMAGES_DIR = "imgs/"
+OUTPUT_DIR = "filtered/"
+
+def main():
+    for _, _, img_filenames in os.walk(IMAGES_DIR):
+        for ind, filename in enumerate(img_filenames):
+            print("Reading the image...")
+            image = img_as_float(color.rgb2gray(imread(IMAGES_DIR + filename)))
+
+            subdir = OUTPUT_DIR + str(ind) + "/"
+            try:
+            	os.makedirs(subdir)
+            except:
+            	print("%s already exists continuing" % subdir)
+
+
+            print("Applying filters and storing...")
+            image_filtered = mean(image, disk(5))
+            imsave(subdir + filename + ".mean.jpg", image_filtered)
+            image_filtered = mean_bilateral(image, disk(5))
+            imsave(subdir + filename + ".mean_bilateral.jpg", image_filtered)
+            image_filtered = median(image, disk(5))
+            imsave(subdir + filename + ".median.jpg", image_filtered)
+
+
+
+if __name__ == "__main__":
+    main()
