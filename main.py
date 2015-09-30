e9cb0a97d2da06c2364896c52474477591dc00e0
Sergey Krivohatskiy <s.krivohatskiy@gmail.com>
2015-09-30 10:18:17 +0300

    Read image implemented
    Signed-off-by: Sergey Krivohatskiy <s.krivohatskiy@gmail.com>

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ def processor_wrapper(input_image_file, out_image_file, image_processor):
     imsave(out_image_file, out_image)
 
 
+def image_name(image_id):
+    return 'w_' + str(image_id) + '.jpg'
+
+
 class ImagesReader(object):
     """
     ImagesReader reads images from specified directory
@@ -30,6 +34,7 @@ class ImagesReader(object):
         self.image_ids = [int(image_name[2:-4]) for image_name in os.listdir(self.dir_path)
                           if image_name.startswith('w_') and image_name.endswith('.jpg')]
         self.image_ids.sort()
+        self.pre_processed_with = None
 
     def pre_process(self, image_processor, rewrite=False, threads=8):
         """
@@ -38,16 +43,16 @@ class ImagesReader(object):
         """
         processor_name = image_processor.__name__
         sys.stdout.write('Pre processing images with "' + processor_name + '" processor\n')
-        pre_processing_dir = os.path.join(os.path.abspath(PRE_PROCESSING_DIR), processor_name)
+        pre_processing_dir = self.pre_processor_dir(processor_name)
         if not os.path.exists(pre_processing_dir):
             os.makedirs(pre_processing_dir)
         process_pool = Pool(threads)
         futures = []
         for idx, image_id in enumerate(self.image_ids):
-            out_image_file = os.path.join(pre_processing_dir, 'w_' + str(image_id) + '.jpg')
+            out_image_file = os.path.join(pre_processing_dir, image_name(image_id))
             if os.path.exists(out_image_file) and not rewrite:
                 continue
-            input_image_file = os.path.join(self.dir_path, 'w_' + str(image_id) + '.jpg')
+            input_image_file = os.path.join(self.dir_path, image_name(image_id))
             args = [input_image_file, out_image_file, image_processor]
             futures.append(process_pool.apply_async(processor_wrapper, args))
 
@@ -58,6 +63,17 @@ class ImagesReader(object):
         process_pool.close()
         process_pool.join()
         sys.stdout.write('\nPre processing finished')
+        self.pre_processed_with = processor_name
+
+    def read_image_vector(self, image_id):
+        if self.pre_processed_with is None:
+            raise RuntimeError('Pre processing should be called first')
+        image = imread(os.path.join(self.pre_processor_dir(self.pre_processed_with), image_name(image_id)))
+        return np.resize(image, image.shape[0] * image.shape[1])
+
+    @staticmethod
+    def pre_processor_dir(processor_name):
+        return os.path.join(os.path.abspath(PRE_PROCESSING_DIR), processor_name)
 
 
 def read_train(f):
@@ -98,14 +114,14 @@ def write_submission(whale_types, image_ids_whale_probabilities, submission_file
     writer = csv.writer(submission_file)
     writer.writerow(['Image'] + list(whale_types))
     for image_id, whale_probabilities in image_ids_whale_probabilities:
-        writer.writerow(['w_' + str(image_id) + '.jpg'] + list(whale_probabilities))
+        writer.writerow([image_name(image_id)] + list(whale_probabilities))
 
 
 def main():
     with open(TRAIN_FILENAME) as train_data_file, open(SUBMISSION_FILENAME, 'wb') as submission_file:
         image_ids_whale_ids, whale_types = read_train(train_data_file)
         images_reader = ImagesReader(IMAGES_DIR)
-        images_reader.pre_process(image_processors.region_crop_gray_downscale, rewrite=True, threads=1)
+        images_reader.pre_process(image_processors.region_crop_gray_downscale, rewrite=False, threads=3)
 
         # TODO replace random submission to a normal one
         train_image_ids = set(image_ids_whale_ids[:, 0])
